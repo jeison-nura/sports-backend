7666d5101b702ee5fba0085a9b75e2a6583fcc78
shirux <shirux123@gmail.com>
2022-09-06 15:49:47 -0500

    added login endpoint

--- user/models.py
+++ user/models.py
@@ -1,4 +1,7 @@
+from collections import UserDict
 from enum import Enum
+from multiprocessing.sharedctypes import Value
+from typing import OrderedDict
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -7,10 +10,35 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser): 
     email = models.EmailField(unique=True)
 
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError('The username must not be empty')
+
+        if not email:
+            raise ValueError('The email must be valie and different than empty')
+        
+        if not password:
+            raise ValueError('The password should not be empty')
+    
+        return User.objects.create(username, email, password, **extra_fields)
+
+    def create_user(self, username: str, email: str, password: str, **extra_fields):
+        """"""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_super_user(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(username, email, password, **extra_fields)
+
 class Genders(Enum):
     MALE = "male"
     FEMALE = "female"
     OTHER = "other"
+
+
 class Profile(models.Model):
     GENDER_CHOICES = (
         ('MALE', Genders.MALE),
--- user/views.py
+++ user/views.py
@@ -1,8 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from user.serializers import RegisterSerializer, UserSerializer
 
-class HolaGuebon(APIView):
-    def get(self, request):
-        return Response({'hola': 'hola guebon'})
+
+
+class RegisterUserView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        print(serializer.data)
+        return Response({
+            "user": UserSerializer(user).data,
+        })
 
